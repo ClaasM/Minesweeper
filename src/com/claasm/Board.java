d7c1f0f8712650b3c71f6678f55f5dfd3cd8ba5e
ga96sag <ga96sag@tum.de>
2017-07-17 19:38:19 +0200

    Implemented the first, very basic game loop and, for that, also some more scaffolding in the Board-class

--- src/com/claasm/Board.java
+++ src/com/claasm/Board.java
@@ -1,8 +1,65 @@
 package com.claasm;
 
+import java.util.Arrays;
+
 /**
  * Created by claasmeiners on 17/07/17.
  */
 public class Board {
     Cell[][] cells;
+
+    public Board(int width, int height) {
+        this.cells = new Cell[width][height];
+        //TODO populate
+    }
+
+    public int getWidth() {
+        return cells.length;
+    }
+
+    public int getHeight() {
+        return cells.length > 0 ? cells[0].length : 0;
+    }
+
+    /**
+     * Simulates the left-click of the user on a cell, opening the cell and all neighboring cells according to Minesweeper rules, or ending the game if there was a bomb below.
+     *
+     * @param x the x-location of the cell
+     * @param y the y-location of the cell
+     * @return true if it was a bomb (the game needs to stopped)
+     */
+    public boolean click(int x, int y) {
+        //TODO
+        return false;
+    }
+
+    /**
+     * Simulates the right-click of the user on a cell, toggling the flag on the cell.
+     *
+     * @param x the x-location of the cell
+     * @param y the y-location of the cell
+     * @return false if it was a bomb (the game needs to stopped)
+     */
+    public void toggleFlag(int x, int y) {
+        //TODO
+    }
+
+    /**
+     * Used to check whether the game is won
+     * @return true if there are no more empty cells uncovered
+     */
+    public boolean isCleared() {
+        //TODO
+        return false;
+    }
+
+    /**
+     * Used to print the board to the console so the user can see the state of the game
+     * @return the board, represented as a human-readable character-grid
+     */
+    @Override
+    public String toString() {
+        //TODO
+        return null;
+    }
 }
--- src/com/claasm/Game.java
+++ src/com/claasm/Game.java
@@ -1,8 +1,57 @@
 package com.claasm;
 
+import java.util.Scanner;
+
 /**
  * Created by claasmeiners on 17/07/17.
  */
 public class Game {
+    //Used to read the user input
+    Scanner sc;
+
+    //The board holds all the pieces
     Board board;
+
+    public Game() {
+        this.board = new Board(10,10);
+        this.sc = new Scanner(System.in);
+    }
+
+    public void start(){
+        boolean isRunning = true;
+        while(isRunning){
+
+            //Get the input for the next turn from the user
+            System.out.println("Which action? c for click, f for flag");
+            String action = sc.nextLine(); //Flag or click
+            System.out.println("Which column?");
+            int x = sc.nextInt(board.getWidth()); //x position of the selected cell
+            System.out.println("Which row");
+            int y = sc.nextInt(board.getHeight()); //y position of the selected cell
+
+            //Perform the appropriate action
+            if("c".equals(action)){
+                //The user left-clicked on a cell
+                boolean wasBomb = board.click(x,y);
+                boolean won = board.isCleared();
+                if(wasBomb){
+                    //The user selected a bomb, end the game
+                    System.out.println("Game Over!");
+                    isRunning = false;
+                } else if (won){
+                    //The user cleared all empty cells
+                    System.out.println("You won!");
+                    isRunning = false;
+                } //else continue
+            } else if ("f".equals(action)){
+                //The user right-clicked on a cell
+                board.toggleFlag(x,y);
+            } else {
+                //The user is allowed to mistype the action, the loop will just start from the beginning in that case
+                System.out.println("Invalid input!");
+            }
+            //Don't do anything in the loop beyond here
+        }
+        System.out.println("Bye Bye!");
+    }
 }
